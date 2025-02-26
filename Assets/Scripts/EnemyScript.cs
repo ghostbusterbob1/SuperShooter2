8c07e333a96886db2a034c0d753149de26521e6f
ghostbusterbob <76821810+GitPushh@users.noreply.github.com>
2025-02-26 16:16:17 +0100

    EnemyShoot

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,15 +9,51 @@ public class EnemyScript : MonoBehaviour
     // Start is called before the first frame update
     NavMeshAgent agent;
     GameObject player;
+    public GameObject projectilePrefab; 
+    public float fireRate = 2f; 
+    public float projectileSpeed = 50f;
+    public float lifeTime = 5f;
+    Transform firePoint;
+
+
+    private float timer;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player");
+        
+        firePoint = transform.Find("firePoint"); // Finds child named "FirePoint"
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(player.transform.position); 
+
+        transform.LookAt(player.transform.position);
+        if (Time.time > timer) 
+        {
+            Shoot();
+            timer = Time.time + 1f / fireRate;
+        }
     }
+
+    private void Shoot()
+    {
+        
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = false;
+            rb.velocity = firePoint.forward * projectileSpeed;
+        }
+
+        Destroy(projectile, lifeTime);
+
+
+    }
+
 }
